e33bd54f43467b879835b587dd35d3ea6750469b
Julienxyz <zhuwentao1@stu.scu.edu.cn>
2024-03-08 16:53:40 +0800

    vue3+vite基本框架界面搭建和功能区块划分

---
+++ vue+vite/my-vue-app/src/components/CommonHeader.vue
@@ -0,0 +1,54 @@
+
+<template>
+  <el-header>
+    <div class="l-content">
+          <el-button size="small">
+            <el-icon :size="20">
+              <Menu />
+            </el-icon>
+          </el-button>
+    </div>
+    <div class="r-content">
+      <el-dropdown>
+    <span class="el-dropdown-link">
+      <img class="user" :src="getImgSrc()" alt="" />
+    </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>个人中心</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </el-header>
+</template>
+<script>
+export default {
+  setup(){
+   // const imgSrc =require('../assets/images/user.png')
+    const  getImgSrc = () => {
+      return new URL("../assets/images/user.png",import.meta.url).href;
+    };
+    return{
+     // imgSrc,
+      getImgSrc
+    };
+  },
+};
+</script>
+<style lang="less" scoped>
+header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+.r-content{
+  .user{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+}
+</style>
---
+++ vue+vite/my-vue-app/src/views/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="common-layout">
+    <el-container class="lay-container">
+      <common-aside/>
+      <el-container class="r-container">
+        <common-header/>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+import CommonHeader from "../components/CommonHeader.vue";
+import CommonAside from "../components/CommonAside.vue";
+export default defineComponent({
+  components:{
+    CommonAside,
+    CommonHeader,
+  },
+});
+</script>
+<style lang="less" scoped>
+.r-container{
+  flex-wrap: wrap;
+}
+
+</style>
